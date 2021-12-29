910f5c18fb58564bfa579c18068d9a81b6af28b1
ZhaoNan <545263557@qq.com>
2021-12-29 17:18:59 +0800

    修改为能适配第二个函数的项目

--- tdd_study/main.go
+++ tdd_study/main.go
@@ -11,11 +11,11 @@ func inputInfo(name string, fatRate float64) {
 
 func getRank(name string) (rank int, fatRate float64) {
 	//我们需要根据体脂进行排名，需要先把里面的键值对反转一下
-	fatRatePerson := map[float64]string{}
+	fatRatePerson := map[float64][]string{}
 	rankArr := make([]float64, 0, len(personFatrate))
 	for nameItem, fatRateItem := range personFatrate {
 		//原来的键变成值
-		fatRatePerson[fatRateItem] = nameItem
+		fatRatePerson[fatRateItem] = append(fatRatePerson[fatRateItem], nameItem)
 		rankArr = append(rankArr, fatRateItem) //只对体脂率进行排名，所以数组中只引入fatRateItem
 	}
 	//需要一个排序函数，然后把每次排序的结果扔到一个数组里
@@ -23,11 +23,14 @@ func getRank(name string) (rank int, fatRate float64) {
 	sort.Float64s(rankArr)
 	//数组的下标是0开头，所以rank的值需要下标+1
 	for i, fatRateItem := range rankArr {
-		_name := fatRatePerson[fatRateItem]
-		if _name == name {
-			rank = i + 1
-			fatRate = fatRateItem
-			return
+		//得到了体脂率对应的name数组
+		_names := fatRatePerson[fatRateItem]
+		for _, _name := range _names {
+			if _name == name {
+				rank = i + 1
+				fatRate = fatRateItem
+				return
+			}
 		}
 	}
 	return 0, 0
